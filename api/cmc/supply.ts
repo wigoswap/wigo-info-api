c13381e24cb3d40a7dcc4cd9d4131cae88668d6d
WigoSwap <wearewigo@protonmail.com>
2022-09-10 12:04:30 +0200

    feat: Remove old CMC Supply API

--- api/cmc/supply.ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { getTotalSupply } from "../../utils/supply";
-
-export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
-  let totalSupply = await getTotalSupply();
-  totalSupply = totalSupply.div(1e18);
-
-  res.send(totalSupply.toNumber());
-};
