3a078b709a6f5748569025f209de3928d5dbd7c1
WigoSwap <wearewigo@protonmail.com>
2024-06-29 10:20:24 +0200

    build: Update thegraph endpoints

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/wigoswap/exchange",
+    uri: "https://gateway-arbitrum.network.thegraph.com/api/5dace53f482f1751985afdebcb310dba/subgraphs/id/ykYhkpzjz9FpT4RZRgFHQ84udU1581pfs4u9KcbA4yt",
   }),
   cache: new InMemoryCache(),
 });
